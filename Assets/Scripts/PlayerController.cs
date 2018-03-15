c1f4c9e04cccfd3a975a62a72e061e7a47b3b1a3
Thomas <Thomasnathanct@gmail.com>
2018-03-15 12:09:15 +0000

    Working on Jumps
    I placed the variable necessary for our jumps to work. The first few conditions are currently being created.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
     public float movespeed;
@@ -14,6 +15,18 @@ public class PlayerController : MonoBehaviour {
     LevelManager LM;
 
 
+    public int FullCanister = 1;
+    public int CurrentCanister1Energy;
+    public int CurrentCanister2Energy;
+    public int CurrentCanister3Energy;
+
+    public Slider Canister1;
+    public Slider Canister2;
+    public Slider Canister3;
+
+    public bool Canister1hasbeenused;
+    public bool Canister2hasbeenused;
+    public bool Canister3hasbeenused;
 
 
 
@@ -25,6 +38,12 @@ public class PlayerController : MonoBehaviour {
         MovementInputValue = 0f;
         LeftRightInputValue = 0f;
         LM = FindObjectOfType<LevelManager>();
+        CurrentCanister1Energy = FullCanister;
+        CurrentCanister2Energy = FullCanister;
+        CurrentCanister3Energy = FullCanister;
+        Canister1hasbeenused = false;
+        Canister2hasbeenused = false;
+        Canister3hasbeenused = false;
  
     }
 
@@ -32,10 +51,20 @@ public class PlayerController : MonoBehaviour {
     {
         MovementAxisName = "Vertical";
         LeftRightAxisName = "Horizontal";
+        CurrentCanister1Energy = FullCanister;
+        CurrentCanister2Energy = FullCanister;
+        CurrentCanister3Energy = FullCanister;
+
     }
 
     private void Update()
     {
+
+        
+
+
+
+
 
         if(LM.IsthereaDrone == true)
         {
@@ -65,4 +94,13 @@ public class PlayerController : MonoBehaviour {
         Vector3 LeftRight = transform.right * LeftRightInputValue * movespeed * Time.deltaTime;
         rb.MovePosition(rb.position + LeftRight);
     }
+
+    private void SetHealthUI()
+    {
+        Canister1.value = CurrentCanister1Energy;
+        Canister2.value = CurrentCanister2Energy;
+        Canister3.value = CurrentCanister3Energy;
+
+    }
+
 }
