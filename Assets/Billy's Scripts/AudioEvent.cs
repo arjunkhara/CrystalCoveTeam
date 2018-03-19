98ecbc73e9b9ab1aec8c36687b9c65bf600d8d61
Billy - Macbook <wkarnchanapee@gmail.com>
2018-03-19 22:22:33 +0000

    Implemented Footstep audio event and Ambience

---
+++ Assets/Billy's Scripts/AudioEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioEvent : MonoBehaviour {
+
+    [FMODUnity.EventRef]
+    public string fmodEvent;
+
+    public FMOD_StudioEventEmitter emitter;
+
+	private void Start()
+	{
+        emitter = GetComponent<FMOD_StudioEventEmitter>();
+	}
+
+	// Update is called once per frame
+	public void PlaySound () {
+        FMODUnity.RuntimeManager.PlayOneShot(fmodEvent);
+	}
+}
---
+++ Assets/Billy's Scripts/FootstepAudio.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class FootstepAudio : AudioEvent
+{
+    [SerializeField] float interval = 0.5f;
+    float timer = 0f;
+
+	// Update is called once per frame
+	void Update()
+    {   timer += Time.deltaTime;
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S)) {
+            print("Hello");
+
+            if (timer > interval)
+            {
+                PlaySound();
+                timer = 0f;
+                print("jjojo");
+            }  
+        }
+
+
+
+	}
+}
