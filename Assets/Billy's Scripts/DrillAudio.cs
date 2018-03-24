db26a7368bb13ef7cb1f871e95ba7c3c1d149451
Billy - Macbook <wkarnchanapee@gmail.com>
2018-03-24 20:37:50 +0000

    added drill audio event

---
+++ Assets/Billy's Scripts/DrillAudio.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+using FMOD.Studio;
+
+
+public class DrillAudio : AudioEvent
+{
+    //[SerializeField] float interval = 0.5f;
+    //float timer = 0f;
+    bool active = false;
+    EventInstance drillEventInst;
+    float drive;
+
+	private void Start()
+	{
+        drillEventInst = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
+        //FMODUnity.RuntimeManager.AttachInstanceToGameObject(drillEventInst, , GetComponent<Rigidbody>());
+	}
+	// Update is called once per frame
+	void Update()
+    {   
+        if (Input.GetMouseButton(1)) 
+        {
+            if (!active) 
+            {
+                drive = 0;
+                drillEventInst.start();
+                active = true;
+            } else {
+                drive += 0.01f;
+                drillEventInst.setParameterValue("drive", drive);
+            }
+        }
+        if (Input.GetMouseButtonUp(1)) 
+        {
+            drillEventInst.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            active = false;
+            
+        }
+	}
+}
