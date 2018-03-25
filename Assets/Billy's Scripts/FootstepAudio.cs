635d7002da220f57aaca30ef92fc5d4521da1046
Thomas <Thomasnathanct@gmail.com>
2018-03-25 23:16:35 +0200

    Fixed the audio Bug
    Fixed the audio bug

--- Assets/Billy's Scripts/FootstepAudio.cs
+++ Assets/Billy's Scripts/FootstepAudio.cs
@@ -5,11 +5,17 @@ public class FootstepAudio : AudioEvent
 {
     [SerializeField] float interval = 0.5f;
     float timer = 0f;
+    LevelManager LM;
 
-	// Update is called once per frame
-	void Update()
+
+    private void Start()
+    {
+        LM = FindObjectOfType<LevelManager>();
+    }
+    // Update is called once per frame
+    void Update()
     {   timer += Time.deltaTime;
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.S)) {
+        if (Input.GetKey(KeyCode.W) && LM.IsthereaDrone == false || Input.GetKey(KeyCode.A) && LM.IsthereaDrone == false|| Input.GetKey(KeyCode.D) && LM.IsthereaDrone == false || Input.GetKey(KeyCode.S) && LM.IsthereaDrone == false) {
             
 
             if (timer > interval)
